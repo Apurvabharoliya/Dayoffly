3fcb6ce3edddb127d87f9f64448423f9067a8233
Dhruvi Patel <156531851+Dhruvi502@users.noreply.github.com>
2025-09-25 20:32:27 +0530

    Add files via upload

---
+++ dayoffly_db.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 25, 2025 at 04:51 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dayoffly_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `department_id` int(5) NOT NULL,
+  `department_name` enum('Human Resources','Finance','IT','Sales','Marketing','Research & Development') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_application`
+--
+
+CREATE TABLE `leave_application` (
+  `leave_id` int(5) NOT NULL,
+  `user_id` int(5) DEFAULT NULL,
+  `leave_type` varchar(30) DEFAULT NULL,
+  `applied_on` datetime DEFAULT current_timestamp(),
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `reason` text NOT NULL,
+  `attachment` varchar(255) DEFAULT NULL,
+  `leave_status` varchar(10) DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_balance`
+--
+
+CREATE TABLE `leave_balance` (
+  `user_id` int(5) DEFAULT NULL,
+  `leave_type` varchar(30) DEFAULT NULL,
+  `total_leaves` int(3) NOT NULL,
+  `used_leaves` int(3) DEFAULT 0,
+  `remaining_leaves` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_types`
+--
+
+CREATE TABLE `leave_types` (
+  `leave_type` varchar(30) NOT NULL,
+  `rules` text DEFAULT NULL,
+  `carry_forward_allowed` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(5) NOT NULL,
+  `role_name` enum('Manager','HR','Senior','Junior','Intern') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_master`
+--
+
+CREATE TABLE `users_master` (
+  `user_id` int(5) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `department_id` int(5) DEFAULT NULL,
+  `role_id` int(5) DEFAULT NULL,
+  `designation` varchar(20) NOT NULL,
+  `contact_number` varchar(16) NOT NULL,
+  `is_active` tinyint(1) DEFAULT 1,
+  `approver_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `leave_application`
+--
+ALTER TABLE `leave_application`
+  ADD PRIMARY KEY (`leave_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `leave_type` (`leave_type`);
+
+--
+-- Indexes for table `leave_balance`
+--
+ALTER TABLE `leave_balance`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `leave_type` (`leave_type`);
+
+--
+-- Indexes for table `leave_types`
+--
+ALTER TABLE `leave_types`
+  ADD PRIMARY KEY (`leave_type`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `users_master`
+--
+ALTER TABLE `users_master`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `approver_id` (`approver_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `leave_application`
+--
+ALTER TABLE `leave_application`
+  MODIFY `leave_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `leave_application`
+--
+ALTER TABLE `leave_application`
+  ADD CONSTRAINT `leave_application_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_master` (`user_id`),
+  ADD CONSTRAINT `leave_application_ibfk_2` FOREIGN KEY (`leave_type`) REFERENCES `leave_types` (`leave_type`);
+
+--
+-- Constraints for table `leave_balance`
+--
+ALTER TABLE `leave_balance`
+  ADD CONSTRAINT `leave_balance_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_master` (`user_id`),
+  ADD CONSTRAINT `leave_balance_ibfk_2` FOREIGN KEY (`leave_type`) REFERENCES `leave_types` (`leave_type`);
+
+--
+-- Constraints for table `users_master`
+--
+ALTER TABLE `users_master`
+  ADD CONSTRAINT `users_master_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
+  ADD CONSTRAINT `users_master_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
+  ADD CONSTRAINT `users_master_ibfk_3` FOREIGN KEY (`approver_id`) REFERENCES `users_master` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
